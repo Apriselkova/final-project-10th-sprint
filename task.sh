5c980684bc6378b6b8e8b4442cecc446e967b5c5
Антонина Приселкова <chigineva89@gmail.com>
2025-08-17 14:59:42 +0300

    second iteration

--- task.sh
+++ task.sh
@@ -18,44 +18,49 @@ cd task
 touch dir2/empty
 
 # создаём файл task/dir2/hello.sh с таким содержанием:
-echo -e '#!/bin/bash\n\necho "$1, привет!"' > task/dir2/hello.sh
+# echo -e '#!/bin/bash\n\necho "$1, привет!"' > task/dir2/hello.sh
+echo -e '#!/bin/bash\necho  "$1, привет"' > dir2/hello.sh
 
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
-chmod 764 task/dir2/hello.sh
+chmod 764 ./dir2/hello.sh
 
 # сохраняем список файлов task/dir2 в task/dir2/list.txt
-ls task/dir2 > task/dir2/list.txt
+touch dir2/list.txt
+ls ./dir2 > ./dir2/list.txt
 
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
-cp -r task/dir2/* task/dir3/dir4/
+cp -r ./dir2/* ./dir3/dir4/
 
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
-find task -type f -name "*.txt" > task/dir1/summary.txt
+touch dir1/summary.txt
+find . -name "*.txt" > ./dir1/summary.txt
 
 # дописываем в task/dir1/summary.txt содержимое task/dir2/list.txt
-cat task/dir2/list.txt >> task/dir1/summary.txt
+cat ./dir2/list.txt >> ./dir1/summary.txt
 
 # определяем переменную окружения NAME со значением "Всем студентам"
 export NAME="Всем студентам"
 
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
-task/dir2/hello.sh "$NAME" >> task/dir1/summary.txt
+./dir2/hello.sh "$NAME" >> ./dir1/summary.txt
 
 # перемещаем с переименованием task/dir1/summary.txt в task/Практическое задание
-mv task/dir1/summary.txt task/Практическое\ задание/summary.txt
+mkdir Практическое\ задание
+mv ./dir1/summary.txt ./Практическое\ задание/summary.txt
 
 # выводим на консоль содержимое файла task/Практическое задание
-echo "$(cat task/Практическое\ задание/summary.txt)"
+echo "$(cat ./Практическое\ задание/summary.txt)"
 
 # ищем в файле "Практическое задание" строки, которые содержат слово "dir"
 # и затем отсортировываем их
-grep "dir" task/Практическое\ задание/summary.txt | sort
+grep "dir" ./Практическое\ задание/summary.txt | sort
 
 # меняем текущую директорию на родительскую для task
-cd "$(dirname "$(realpath task)")"
+# cd "$(dirname "$(realpath task)")"
+cd ..
 
 # удаляем директорию task со всем содержимым
-rm -rf task
+# rm -rf task
 
